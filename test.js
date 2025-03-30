8dfcd179f1459b3566e33b1363b261eb58596673
arnobrahat <arnobrahat@gmail.com>
2025-03-30 22:39:23 +0600

    new update in file name and contexts

---
+++ test.js
@@ -0,0 +1,7 @@
+//push code to GitHub.
+//more lines to the file
+
+git init // initiate the git repository
+git add . // add all files to the staging area  
+git commit -m "first commit" // commit the changes with a message
+git branch -M main // rename the default branch to main
