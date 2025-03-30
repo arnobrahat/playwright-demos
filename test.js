23cc342ce87057134235fb406c45344a17bfb69b
arnobrahat <arnobrahat@gmail.com>
2025-03-30 23:14:17 +0600

    new update

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 //push code to GitHub.
 //more lines to the file
 
-git init // initiate the git repository
-git add . // add all files to the staging area  
-git commit -m "first commit" // commit the changes with a message
-git branch -M main // rename the default branch to main
+git init // initiate the git repository into the project folder
+git ls -al // check all the hidden files in the project folder
+// and check the .git folder is created or not.
+git 
+git status // check the status of the repository
+git add  // git add follwed by the file name // add a specific file to the staging area
+git commit -m "first commit" test.js // commit the changes with a comment followed by the file name
+git push -u origin main or master // push the changes to the remote repository branch
+// if the branch name is main, use main instead of master
+git branch -M main // rename the default branch to main 
