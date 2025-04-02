31bc8fe94d4a29b0b4babdf4f389fb8a82d85237
arnob rahat <46374422+arnobrahat@users.noreply.github.com>
2025-04-02 20:30:09 +0600

    Update test.js

--- test.js
+++ test.js
@@ -4,9 +4,9 @@
 git init // initiate the git repository into the project folder
 git ls -al // check all the hidden files in the project folder
 // and check the .git folder is created or not.
-git 
 git status // check the status of the repository
-git add  // git add follwed by the file name // add a specific file to the staging area
+git add  // git add followed by the file name // add a specific file to the staging area
+git add . // add all the files 
 git commit -m "first commit" test.js // commit the changes with a comment followed by the file name
 git push -u origin main or master // push the changes to the remote repository branch
 // if the branch name is main, use main instead of master
