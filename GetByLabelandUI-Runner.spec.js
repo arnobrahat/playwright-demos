dc46a88b6d2f4ca382f080fc8505a95a9dc14dd1
Rahat <arnobrahat@gmail.com>
2025-04-09 03:28:02 +0600

    getByPlaceholder, getByRole, getByText locator implemented

--- GetByLabelandUI-Runner.spec.js
+++ GetByLabelandUI-Runner.spec.js
@@ -7,7 +7,30 @@ test ('Usage of getByLabel locator and use of UI Runner', async({page})=>{
     await page.getByLabel("Check me out if you Love IceCreams!").click(); //click for checkboxed
     await page.getByLabel("Employed").check(); //check for radio button
     await page.getByLabel("Gender").selectOption("Female"); // selectOption is only valid when the options are within the select tag
-})
+
+    await page.getByPlaceholder("Password").fill("123456"); 
+    /*
+        when placeholder attribute is present, 
+        use getByPlaceholder to select the element
+    */
+
+    await page.getByRole("button",{name:"Submit"}).click();
+    /*
+        when button tag or class name btn is present, 
+        use getByRole("button") followed by the button value to select the element
+    */
+
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    /*
+        when getByText() locator is used, 
+        it scans for all the texts in the page and fetch for the text mentioned.
+    */
+
+    // Navigating to the shop page
+    await page.getByRole("link",{name: "Shop"}).click();
+    await page.locator("app-card").filter({hasText:"Nokia Edge"}).getByRole("button").click();
+
+});
 
 
 // playwrite runner is opned when the below command is given in the terminal
