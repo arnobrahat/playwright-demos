33e405211bd5bb68facbd4ca32a89f4e3ef9d2ea
Rahat <arnobrahat@gmail.com>
2025-04-10 10:08:32 +0600

    final push

--- tests/ClientWebsite.spec.js
+++ tests/ClientWebsite.spec.js
@@ -2,11 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test ("Website login", async({page})=>{
 
+    const email = "anshika@gmail.com";
     const products = page.locator(".card-body");
     const productName = "ZARA COAT 3"
 
     await page.goto ("https://rahulshettyacademy.com/client");
-    await page.locator('#userEmail').fill('anshika@gmail.com');
+    await page.locator('#userEmail').fill(email);
     await page.locator("#userPassword").fill('Iamking@000');
     await page.locator('#login').click();
 
@@ -28,8 +29,13 @@ test ("Website login", async({page})=>{
     await page.locator("div li").first().waitFor(); // this will wait for the first locator to be visible
     const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
     expect(bool).toBeTruthy();
+    //console.log('ZARA COAT 3');
 
-    // Filling the checkout information
+     // Verifying the email address in checkout is same as login 
+    expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    //console.log(email);
+
+    // Finding the country name
     await page.locator("text=Checkout").click();
     await page.locator("[placeholder*='Country']").pressSequentially("ind"); // this method types the inputs one by one.
     
@@ -43,8 +49,31 @@ test ("Website login", async({page})=>{
             break;
         }
     }
-    await page.pause();
+    await page.locator(".action__submit").click();
+    await expect (page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+
+    const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log('Order id: ', orderID);
+
+    // Searching the order id from the order list table
+
+    await page.locator("[routerlink*='myorders']").first().click();
+    await page.locator("tbody").waitFor(); // waiting for the table body to load
+
+    const tableRows = page.locator("tbody tr");
+    for (let i=0; i<tableRows.count();++i)
+    {
+        const rowOrderId = await tableRows.nth(i).locator("th").textContent(); // from the whole row going to the order id header
+        if (orderID.includes(rowOrderId))
+        {
+            await tableRows.nth(i).locator("button").first().click(); // selecting the first button
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text.-main").isVisible();
     
+    
+    expect (orderID.includes(orderIdDetails)).toBeTruthy();
 });
 
 
---
+++ tests/GetByLabelandUI-Runner.spec.js
@@ -0,0 +1,37 @@
+import {test, expect} from '@playwright/test';
+
+test ('Usage of getByLabel locator and use of UI Runner', async({page})=>{
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+    // when any text is written inside the Label tag, we can direct to that point using the getByLabel locator
+    await page.getByLabel("Check me out if you Love IceCreams!").click(); //click for checkboxed
+    await page.getByLabel("Employed").check(); //check for radio button
+    await page.getByLabel("Gender").selectOption("Female"); // selectOption is only valid when the options are within the select tag
+
+    await page.getByPlaceholder("Password").fill("123456"); 
+    /*
+        when placeholder attribute is present, 
+        use getByPlaceholder to select the element
+    */
+
+    await page.getByRole("button",{name:"Submit"}).click();
+    /*
+        when button tag or class name btn is present, 
+        use getByRole("button") followed by the button value to select the element
+    */
+
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    /*
+        when getByText() locator is used, 
+        it scans for all the texts in the page and fetch for the text mentioned.
+    */
+
+    // Navigating to the shop page
+    await page.getByRole("link",{name: "Shop"}).click();
+    await page.locator("app-card").filter({hasText:"Nokia Edge"}).getByRole("button").click();
+
+});
+
+
+// playwrite runner is opned when the below command is given in the terminals
+//npx playwright test GetByLabelandUI-Runner.spec.js --ui    
